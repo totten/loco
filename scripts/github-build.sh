2d811ff37ce5bdadd94120aece6a6fec97f50bf0
Tim Otten <totten@civicrm.org>
2020-06-17 23:24:24 -0700

    Add scripts/github-build.sh

---
+++ scripts/github-build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+## This script generates loco.phar.
+## NOTE: as written, it will *not* work within nix-shell, but it will work on non-nix environments.
+
+## Determine the absolute path of the directory with the file
+## usage: absdirname <file-path>
+function absdirname() {
+  pushd $(dirname $0) >> /dev/null
+    pwd
+  popd >> /dev/null
+}
+
+SCRDIR=$(absdirname "$0")
+PRJDIR=$(dirname "$SCRDIR")
+export PATH="$PRJDIR/bin:$PATH"
+
+set -ex
+composer install --prefer-dist --no-progress --no-suggest --no-dev
+which box
+php -d phar.read_only=0 `which box` build -v
