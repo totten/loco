cc6102d9e84c235d74bc2923d61e47798b2f54a2
Tim Otten <totten@civicrm.org>
2022-11-04 16:06:56 -0700

    nix-builder - Remove bloat
    Some of this was added becase `composer-patches` recommends installing from source. But we don't need full source for everything in the final build...

--- scripts/nix-builder.sh
+++ scripts/nix-builder.sh
@@ -9,6 +9,10 @@ buildPhase() {
   sed -i "s;@package_version@;$package_version;" src/*.php
   env COMPOSER_HOME=$TEMPDIR/composer_home COMPOSER_CACHE_DIR=$TEMPDIR/composer_cache \
     composer install --optimize-autoloader
+  find vendor -name .git | while read gitdir ; do
+    [ -d "$gitdir" ] && rm -rf "$gitdir"
+  done
+  rm -rf vendor/symfony/*/Tests
 }
 
 installPhase() {
